c7cdaf55dd25a1b6fa501d7428b63acdb276f203
Dayane Perez Bravo <65370199+dayanebravo@users.noreply.github.com>
2025-07-04 20:27:32 -0300

    Update aula_03.R

--- aula_03/aula_03.R
+++ aula_03/aula_03.R
@@ -1,6 +1,6 @@
-########################### TEMA 01 - SELEÇÃO CONDICIONAL ######################
+########################### TEMA 01 - SELECAO CONDICIONAL ######################
 
-### Seleção numérica
+### Selecao numerica
 dados <- c(5, 10, 42, 28, 79)
 condicao_selecao <- dados > 15
 
@@ -9,12 +9,12 @@ print(dados[condicao_selecao])
 print(condicao_selecao)
 
 
-### Expressões regulares
-# seleção de caracteres
+### Expressoes regulares
+# selecao de caracteres
 nomes <- c("Ana", "Amanda", "Carlos", "Aline", "Olivia", "Fernando")  
 
 
-# no início da palavra
+# no inicio da palavra
 seleciona_C <- grepl("^C", nomes)
 print(seleciona_C)
 print(nomes[seleciona_C])
@@ -26,7 +26,7 @@ print(seleciona_a)
 print(nomes[seleciona_a])
 
 
-# no início e no final da palavra
+# no inicio e no final da palavra
 seleciona_A_a <- grepl("^A.*a$", nomes)  
 print(seleciona_A_a)  
 print(nomes[seleciona_A_a])  
@@ -39,15 +39,15 @@ print(nomes[seleciona_o])
 
 
 
-### Combinando seleções
-# interseção de seleções - conector "e"
+### Combinando selecoes
+# intersecao de selecoes - conector "e"
 idades <- c(12, 25, 30, 15, 18, 22, 28)
 seleciona_idades_e <- idades >= 20 & idades <= 25
 print(seleciona_idades_e)
 print(idades[seleciona_idades_e])
 
 
-# união de seleções - conector "ou"
+# uniao de selecoes - conector "ou"
 seleciona_idades_ou <- idades < 15 | idades > 25
 print(seleciona_idades_ou)
 print(idades[seleciona_idades_ou])
@@ -62,14 +62,14 @@ print(seleciona_quantidade_7)
 print(frutas[seleciona_quantidade_7]) 
 
 
-# função “%in%” 
+# funcao %in%
 frutas_preferidas <- c("banana", "uva", "goiaba")
 print(frutas_preferidas)
 print(frutas[frutas %in% frutas_preferidas])
 
 
 
-### Seleção Condicional em DataFrames
+### Selecao Condicional em DataFrames
 # Criando o dataframe do estoque de frutas
 frutas <- c("caqui", "banana", "laranja", "uva", "melancia")
 quantidade_frutas <- c(10, 5, 8, 15, 3)
@@ -84,18 +84,18 @@ estoque <- data.frame(
 print(estoque)
 
 
-# Combinando seleções em colunas diferentes
+# Combinando selecoes em colunas diferentes
 quantidade_7 <- estoque$quantidade > 7
 print(quantidade_7)
 
 preco_4 <- estoque$preco < 4
 print(preco_4)
 
-# Aplicando as condições no dataframe
+# Aplicando as condicoes no dataframe
 print(estoque[quantidade_7 & preco_4, ])
 
 
-# Função subset
+# Funcao subset
 subset(x = estoque, subset = quantidade > 7)
 
 subset(estoque, quantidade > 7 & preco < 4)
@@ -107,7 +107,7 @@ subset(estoque, quantidade > 7, select = seleciona_coluna)
 
 
 
-########################### TEMA 02 - ESTRUTURAS DE REPETIÇÃO ##################
+########################### TEMA 02 - ESTRUTURAS DE REPETICAO ##################
 
 # for
 for (i in 1:5) {
@@ -131,10 +131,10 @@ notas <- data.frame(
   prova_3 = c(7, 8, 6)
 )
 
-# Criando uma coluna para armazenar as médias
+# Criando uma coluna para armazenar as medias
 notas$media <- 0  
 
-# Indicando quais colunas estão as notas das provas
+# Indicando quais colunas estao as notas das provas
 colunas_provas <- c("prova_1", "prova_2", "prova_3")   
 
 for (i in 1:nrow(notas)) {
@@ -144,7 +144,7 @@ for (i in 1:nrow(notas)) {
 print(notas)
 
 
-# Função for aninhados
+# Funcao for aninhados
 # Criando uma matriz com todos os elementos iguais a zero
 matriz_3x3 <- matrix(0, nrow = 3, ncol = 3)
 
@@ -161,13 +161,13 @@ print(matriz_3x3)
 
 
 
-########################### TEMA 03 - ESTRUTURAS DE SELEÇÃO ####################
+########################### TEMA 03 - ESTRUTURAS DE SELECAO ####################
 
 # if
 x <- 5
 
 if (x > 0) {
-  print("O número é positivo.")
+  print("O numero eh positivo.")
 }
 
 
@@ -175,9 +175,9 @@ if (x > 0) {
 x <- -3
 
 if (x > 0) {
-  print("O número é positivo.")
+  print("O numero eh positivo.")
 } else {
-  print("O número não é positivo.")
+  print("O numero nao eh positivo.")
 }
 
 
@@ -185,43 +185,43 @@ if (x > 0) {
 x <- 0
 
 if (x > 0) {
-  print("O número é positivo.")
+  print("O numero eh positivo.")
 } else if (x < 0) {
-  print("O número é negativo.")
+  print("O numero eh negativo.")
 } else {
-  print("O número é zero.")
+  print("O numero eh zero.")
 }
 
 
-# ESSE COMANDO NÃO É EXECUTADO
+# ESSE COMANDO NaO eh EXECUTADO
 x <- c(1, -2, 3)
 
 if (x > 0) {
-  print("Pelo menos um número é positivo.")
+  print("Pelo menos um numero eh positivo.")
 }
 # Error in if (x > 0) { : the condition has length > 1
-# existem duas condições verdadeiras
+# existem duas condicoes verdadeiras
 # o if retorna apenas uma
 
 
 # Para contornar esse problema:
-# ifelse(condição, valor se verdadeiro, valor se falso)
+# ifelse(condicao, valor se verdadeiro, valor se falso)
 resultado <- ifelse(x > 0, "Positivo", "Negativo ou zero")
 print(resultado)
 
 
 
-### Seleção 
+### Selecao 
 # if em dataframes
 notas <- data.frame(
   aluno = c("Joao", "Maria", "Pedro"),
   media = c(70, 80, 50)
 )
 
-# Criando uma coluna para a situação do aluno
+# Criando uma coluna para a situacao do aluno
 notas$situacao <- NA  
 
-# Criando as condições de aprovação
+# Criando as condicoes de aprovacao
 for (i in 1:nrow(notas)) {
   
   if (notas$media[i] >= 70) {
@@ -235,17 +235,17 @@ for (i in 1:nrow(notas)) {
 print(notas)
 
 
-# if em funções
+# if em funcoes
 soma <- function(a, b) {
   return(a + b)
 }
 
 soma_3_5 <- soma(3, 5)
 
-if (soma_3_5 %% 2 == 0) {  # resto da divisão = 0 -> par
-  print("A soma é par.")
+if (soma_3_5 %% 2 == 0) {  # resto da divisao = 0 -> par
+  print("A soma eh par.")
 } else {
-  print("A soma é ímpar.")
+  print("A soma eh impar.")
 }
 
 
@@ -267,7 +267,7 @@ for (i in 1:5) {
   }
   print(i)
 }
-# A iteração onde i == 2 é pulada e executada até o último item i==5.
+# A iteracao onde i == 2 eh pulada e executada ate o ultimo item i==5.
 
 
 
@@ -285,7 +285,7 @@ while (contador > 0) {
 
 
 # while com auxiliar
-# variável auxiliar para executar a soma
+# variavel auxiliar para executar a soma
 acumula_soma <- 0
 i <- 1
 
@@ -297,14 +297,14 @@ while (i <= 10) {
 print(acumula_soma)
 
 
-# while aleatório
-# encontrar o primeiro número maior que 100 
-# em uma sequência de números aleatórios
-set.seed(519)  # para reprodução
+# while aleatorio
+# encontrar o primeiro numero maior que 100 
+# em uma sequencia de numeros aleatorios
+set.seed(519)  # para reproducao
 numero <- 0
 
 while (numero <= 100) {
-  numero <- sample(1:200, 1)  # Gera um número aleatório entre 1 e 200
+  numero <- sample(1:200, 1)  # Gera um numero aleatorio entre 1 e 200
   print(numero)
 }
 
@@ -312,21 +312,21 @@ while (numero <= 100) {
 
 ### Combinando while com if
 # while com break
-numero_secreto <- 42 # Este é o número que irá encerrar o loop
+numero_secreto <- 42 # Este eh o numero que ira encerrar o loop
 entrada_usuario <- 0
 
 while (TRUE) {  
-  entrada_usuario <- as.numeric(readline("Adivinhe o número secreto (1 a 50): "))
+  entrada_usuario <- as.numeric(readline("Adivinhe o numero secreto (1 a 50): "))
   
   if (entrada_usuario == numero_secreto) {
-    print("Parabéns! Você acertou!")
+    print("Parabens! Voce acertou!")
     break  # Interrompe o loop
     
   } else if (entrada_usuario < numero_secreto) {
-    print("Tente um número maior.")
+    print("Tente um numero maior.")
     
   } else {
-    print("Tente um número menor.")
+    print("Tente um numero menor.")
   }
   
 }
@@ -337,9 +337,9 @@ i <- 1
 
 while (i <= 10) {
   
-  if (i %% 2 != 0) {  # Verifica se o número é ímpar
+  if (i %% 2 != 0) {  # Verifica se o numero eh impar
     i <- i + 1
-    next  # Pula para a próxima iteração
+    next  # Pula para a proxima iteracao
   }
   
   print(i)
@@ -352,8 +352,8 @@ while (i <= 10) {
 
 ########################### TEMA 05 - A ESTRUTURA REPEAT #######################
 
-# condição de parada interna via if
-# no while é externa no cabeçalho
+# condicao de parada interna via if
+# no while fica externa no cabecalho
 contador <- 5
 
 repeat {
@@ -378,23 +378,23 @@ repeat {
     break
   }
 }
-# quando i == 11, a condição i > 10 se torna verdadeira
+# quando i == 11, a condicao i > 10 se torna verdadeira
 # e o break interrompe o loop
 
 print(acumula_soma)
 
 
 
-### next para pular para a próxima iteração sem executar o restante do código
+### next para pular para a proxima iteracao sem executar o restante do codigo
 i <- 1
 repeat {
   if (i > 10) {
     break
   }
   
-  if (i %% 2 != 0) {  # Verifica se o número é ímpar
+  if (i %% 2 != 0) {  # Verifica se o numero eh impar
     i <- i + 1
-    next  # Pula para a próxima iteração
+    next  # Pula para a proxima iteracao
   }
   
   print(i)
@@ -405,26 +405,25 @@ repeat {
 # NUMERO SECRETO COM REPEAT
 numero_secreto <- 42
 tentativas <- 0
-max_tentativas <- 4   # condição de parada
+max_tentativas <- 4   # condicao de parada
 
 repeat {
-  tentativa <- as.numeric(readline("Adivinhe o número secreto (1 a 100): "))
+  tentativa <- as.numeric(readline("Adivinhe o numero secreto (1 a 100): "))
   tentativas <- tentativas + 1
   
   if (tentativa == numero_secreto) {
-    print("Parabéns! Você acertou!")
+    print("Parabens! Voce acertou!")
     break
     
   } else if (tentativas >= max_tentativas) {
-    print(paste("Você excedeu o número máximo de tentativas.
-                  O número secreto era:", numero_secreto))
+    print(paste("Voce excedeu o numero maximo de tentativas.
+                  O numero secreto era:", numero_secreto))
     break
     
   } else if (tentativa < numero_secreto) {
-    print("Tente um número maior.")
+    print("Tente um numero maior.")
     
   } else {
-    print("Tente um número menor.")
+    print("Tente um numero menor.")
   }
 }
-
